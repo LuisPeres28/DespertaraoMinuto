46102079b754b43b3a3d0bfe8b02bd5d549a4289
LuisPeres28 <euestoudesperto@gmail.com>
2025-12-12 12:23:39 +0000

    Updated index.ts

--- supabase/functions/easypay-mbway/index.ts
+++ supabase/functions/easypay-mbway/index.ts
@@ -13,8 +13,8 @@ serve(async (req) => {
   try {
     const { action, amount, phoneNumber, paymentId } = await req.json()
 
-    const accountId = 'bafe2a58-734e-43ff-9a87-19e28202f01b'
-    const apiKey = '04f21956-25e6-4ca9-b8b9-c1182792b19a'
+    const accountId = 'ba41236b-b132-4c82-bd06-ad4f6d33a6d4'
+    const apiKey = '65f2bcb-d572-41f5-811e-38f6c5d3ef13'
 
     if (action === 'create') {
       const payload = {
